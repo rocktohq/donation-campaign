f5473770884b1cf55b982eeeb33b74baa1b93d59
Saidul Mursalin <itzmonir@gmail.com>
2023-09-24 23:54:59 +0600

    05: Demo Statistics Added

--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -1,7 +1,46 @@
+import { PieChart, Pie, Cell, Legend } from 'recharts';
+
+
 const Statistics = () => {
+  const data = [
+    { name: 'Total Donation', value: 20 },
+    { name: 'Your Donation', value: 4 }
+  ];
+
+  const COLORS = ['#FF444A', '#00C49F'];
+  const RADIAN = Math.PI / 180;
+
+  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
   return (
-    <div>
-      Statistics
+    <div className="flex justify-center items-center text-center my-10">
+      <PieChart width={300} height={300}>
+        <Pie
+          data={data}
+          cx="50%"
+          cy="50%"
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={120}
+          fill="#8884d8"
+          dataKey="value"
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Legend></Legend>
+      </PieChart>
     </div>
   )
 }
