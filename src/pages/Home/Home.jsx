322205e8cd6fcf7cca5dd24d458931fa38a9b8d1
Saidul Mursalin <itzmonir@gmail.com>
2023-09-25 15:07:23 +0600

    Search Option Added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,35 @@
 import { useLoaderData } from "react-router-dom";
 import Banner from "../../components/Header/Banner/Banner";
 import DonationCard from "../../components/DonationCard/DonationCard";
+import { useState } from "react";
 
 const Home = () => {
 
+  let allDonations = [];
   const donations = useLoaderData();
+  const [search, setSearch] = useState(null);
+  // console.log(search)
+
+  search
+    ? (allDonations = donations.filter(donation => donation.category.toLowerCase().includes(search.toLowerCase())))
+    : (allDonations = [...donations])
 
   return (
     <main>
-      <Banner></Banner>
+      <Banner setSearch={setSearch}></Banner>
       <div className="max-w-[1320px] mx-auto px-3">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 my-12">
-          {
-            donations?.map(donation => <DonationCard key={donation.id} donation={donation}></DonationCard>)
-          }
-        </div>
+
+        {
+          allDonations.length > 0 ? (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 my-12">
+              {
+                allDonations.map(donation => <DonationCard key={donation.id} donation={donation}></DonationCard>)
+              }
+            </div>
+          )
+            : <div className="text-center my-10 text-3xl font-bold">No Data Found!</div>
+        }
+
       </div>
     </main>
   )
