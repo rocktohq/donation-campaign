4d95a4fd7f2d08cbdaa8645c3719d5faef650db2
Saidul Mursalin <itzmonir@gmail.com>
2023-09-25 15:20:02 +0600

    ErrorPage Updated

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom"
 
 const ErrorPage = () => {
   return (
-    <div className="h-screen flex flex-col justify-center items-center text-center">
-      <h1 className="text-5xl font-bold">Error: 404 Not Found!</h1>
+    <div className="h-screen flex flex-col justify-center items-center text-center p-3">
+      <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold">Error: 404 Not Found!</h1>
       <p className="mt-5">The page you are looking for, could not be found!</p>
       <Link to="/"><button className="btn btn-error rounded text-white mt-10">Go to Home Page</button></Link>
     </div>
