7c3f3a4105a5cfa638ee665f0824adaf2e501619
Saidul Mursalin <itzmonir@gmail.com>
2023-09-26 08:15:48 +0600

    Donation Ammount System Updated

--- src/pages/DonationCard/DonationCard.jsx
+++ src/pages/DonationCard/DonationCard.jsx
@@ -12,7 +12,7 @@ const DonationCard = ({ donation }) => {
       <div className="p-5">
         <span className="text-sm capitalize rounded-md px-2 py-1" style={{ backgroundColor: category_bg, color: text_button_bg }}>{category}</span>
         <h2 className="text-xl md:text-2xl font-semibold my-3">{title}</h2>
-        <p className="font-semibold" style={{ color: text_button_bg }}>${price}</p>
+        <p className="font-semibold" style={{ color: text_button_bg }}>${parseFloat(price).toFixed(2)}</p>
         <Link to={`/donations/${id}`} state={title}><button className="btn text-white mt-5" style={{ backgroundColor: text_button_bg }}>View Details</button></Link>
       </div>
     </div>
